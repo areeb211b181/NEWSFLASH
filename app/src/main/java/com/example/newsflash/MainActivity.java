ce9af48c7b9ea6bf738a1949166b4c6325494e34
singhkshitij4545 <stayconnectedwithmanish@gmail.com>
2023-05-12 20:39:39 +0530

    adding RVmodal

--- app/src/main/java/com/example/newsflash/MainActivity.java
+++ app/src/main/java/com/example/newsflash/MainActivity.java
@@ -1,14 +1,29 @@
 package com.example.newsflash;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
-}
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class MainActivity extends AppCompatActivity implements CategoryRVAdapter.CategoryClickInterface{
+    //3c9c535e8ea84afb8a3f154c87b2ad61
+
+    private RecyclerView newsRV,categoryRV;
+    private ProgressBar loadingPB;
+    private ArrayList<Articles> articlesArrayList;
+    private ArrayList<CategoryRVModal> categoryRVModalArrayList;
+    private CategoryRVAdapter categoryRVAdapter;
+    private NewsRVAdapter newsRVAdapter;
+
